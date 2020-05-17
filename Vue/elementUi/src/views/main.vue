bbed7fbf6bad20ac881234085a2372ed10267956
Y567 <1592221415@qq.com>
2020-05-17 16:30:32 +0800

    Vue的路由钩子以及异步请求的练习

--- Vue/elementUi/src/views/main.vue
+++ Vue/elementUi/src/views/main.vue
@@ -7,7 +7,29 @@
 <script>
 
 export default {
-  name: 'main'
+  name: 'main',
+  beforeRouteEnter:(to,from,next)=>{
+      console.log("进入组件之前");
+      //记得要继续往下走,不然就卡住了,没有参数表示通行,有路径则走固定路由,false则不放行,返回上一个页面
+      next(vm=>{
+         vm.getData();
+      });
+  },
+  beforeRouteLeave:(to,from,next)=>{
+      console.log("离开组件之前");
+      //往下走
+      next();
+  },
+  methods: {
+      getData: function(){
+          this.axios({
+            method: 'get',
+            url: "http://localhost:8080/static/mock/ag.json"
+          }).then(function(response){
+            console.log(response);
+          });
+      }
+  }
 }
 </script>
 
